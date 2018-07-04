e724a9583f56d2a171d8922f59ce114d78ba970a
r-mittal <rachitmittal312@gmail.com>
2018-07-04 10:54:52 +0530

    Error Handling

---
+++ handling.py
@@ -0,0 +1,14 @@
+
+try:
+    i = 0/0
+    print(i)
+
+    f = open("abc.txt")
+
+    #for x in f:
+     #   print(x)
+except ZeroDivisionError:
+    print("divided by zer0")
+except Exception:
+
+    print("file not foun
